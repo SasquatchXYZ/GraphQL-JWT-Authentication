3439164ef6fc621fd2e8bcc0448ed24f5a366808
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2024-12-06 22:13:13 -0500

    Initial commit

---
+++ src/main/java/com/playground/GraphQlJwtAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphQlJwtAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphQlJwtAuthenticationApplication.class, args);
+	}
+
+}
